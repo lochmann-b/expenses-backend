de3c96fba36baef52be68ee19ed4ffc3708f2ad3
Benjamin Lochmann <benjamin.lochmann@wuerth-phoenix.com>
2019-08-16 14:55:01 +0200

    cors disabled

--- src/main/java/org/benni/expensesbackend/security/SecurityConfiguration.java
+++ src/main/java/org/benni/expensesbackend/security/SecurityConfiguration.java
@@ -32,7 +32,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and()
+        http.cors().disable()
             .csrf().disable()
             .authorizeRequests()
             .antMatchers("/api/user").permitAll()
