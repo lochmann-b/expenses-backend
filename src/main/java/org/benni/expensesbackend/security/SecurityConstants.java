ac38e6f61dea945fff50aea2ed577dbea23e94b3
Benjamin Lochmann <benjamin.lochmann@wuerth-phoenix.com>
2019-08-16 13:17:22 +0200

    jwt secret externalized

--- src/main/java/org/benni/expensesbackend/security/SecurityConstants.java
+++ src/main/java/org/benni/expensesbackend/security/SecurityConstants.java
@@ -7,7 +7,7 @@ public final class SecurityConstants {
 	// Signing key for HS512 algorithm
 	// You can use the page http://www.allkeysgenerator.com/ to generate all kinds
 	// of keys
-	public static final String JWT_SECRET = "n2r5u8x/A%D*G-KaPdSgVkYp3s6v9y$B&E(H+MbQeThWmZq4t7w!z%C*F-J@NcRf";
+	public static final String JWT_SECRET = System.getenv("JWT_SECRET");
 
 	// JWT token defaults
 	public static final String TOKEN_HEADER = "Authorization";
