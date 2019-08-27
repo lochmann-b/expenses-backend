406dc869bc5b2668379febb061ab3821e3187e30
Benjamin Lochmann <benjamin.lochmann@wuerth-phoenix.com>
2019-08-27 09:04:36 +0200

    PUT added to allowed CORS methods

--- src/main/java/org/benni/expensesbackend/security/SecurityConfiguration.java
+++ src/main/java/org/benni/expensesbackend/security/SecurityConfiguration.java
@@ -55,6 +55,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration corsConfig = new CorsConfiguration().applyPermitDefaultValues();
         corsConfig.addAllowedMethod(HttpMethod.DELETE);
+        corsConfig.addAllowedMethod(HttpMethod.PUT);
         source.registerCorsConfiguration("/**", corsConfig);
 
         return source;
